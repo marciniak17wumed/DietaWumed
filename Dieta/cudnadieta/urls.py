b39406e3e009984208c504f0327a590865440e2c
Marcin Łukawski <151506879+marciniak17wumed@users.noreply.github.com>
2023-11-21 14:14:32 +0100

    Update urls.py
    url do wylogowania

--- Dieta/cudnadieta/urls.py
+++ Dieta/cudnadieta/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import home_view, SignUpView, kalk_view, info_view, przepisy_view, kalorie_view
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path('info/', info_view, name='info'),
     path('przepisy/', przepisy_view, name='przepisy'),
     path('kalorie/', kalorie_view, name='kalorie'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
